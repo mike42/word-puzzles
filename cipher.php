799939dbae0b39498c0c493c44f678693e99a80c
Michael Billington <michael.billington@gmail.com>
2016-05-21 20:25:04 +1000

    Auto-format some files, add to gitignore and add .travis.yml with syntax check only

--- cipher.php
+++ cipher.php
@@ -4,49 +4,62 @@ require_once("common/find-a-word.php");
 $cipher = new cls_cipher;
 
 /* Interface here largely copied from from index.php */
-if(has('submit')) {
-	if(has('text') && has('lang')) {
-		if(has('hint'))		{ $req_hint = true;		} else { $req_hint = false; }
-		if(has('symbol'))	{ $req_symbol = true;	} else { $req_symbol = false; }
+if (has('submit')) {
+    if (has('text') && has('lang')) {
+        if (has('hint')) {
+            $req_hint = true;
+        } else {
+            $req_hint = false;
+        }
+        if (has('symbol')) {
+            $req_symbol = true;
+        } else {
+            $req_symbol = false;
+        }
 
-		$req_text = take('text');
-		if(mb_strlen($req_text) > $cipher -> c_max_len) {
-			die("Can't make cryptogram that long.");
-		}
+        $req_text = take('text');
+        if (mb_strlen($req_text) > $cipher -> c_max_len) {
+            die("Can't make cryptogram that long.");
+        }
 
-		$req_lang= take('lang');
-		if(!isset($fw_lang[$req_lang])) {
-			die("Couldn't find that language!");
-		}
+        $req_lang= take('lang');
+        if (!isset($fw_lang[$req_lang])) {
+            die("Couldn't find that language!");
+        }
 
-		$do = "generate"; /* All info to make puzzle. */
-	} else {
-		$do = "nothing";
-	}
+        $do = "generate"; /* All info to make puzzle. */
+    } else {
+        $do = "nothing";
+    }
 } else {
-	$do = "nothing";
+    $do = "nothing";
 }
 
 $page_title    = "Mike's Word Cipher";
-switch($do) {
-	case "nothing":
-		$page_script = "cipher-main";
-		break;
-	case "generate":
-		$cipher -> generate_key('en');
-		$cipher -> hint = $req_hint;
-		$cipher -> symbol = $req_symbol;
-		$cipher -> encode($req_text);
-		$page_script = "cipher-generate";
-		break;
-	default:
-		die("Didn't recognise: $do");
+switch ($do) {
+    case "nothing":
+        $page_script = "cipher-main";
+        break;
+    case "generate":
+        $cipher -> generate_key('en');
+        $cipher -> hint = $req_hint;
+        $cipher -> symbol = $req_symbol;
+        $cipher -> encode($req_text);
+        $page_script = "cipher-generate";
+        break;
+    default:
+        die("Didn't recognise: $do");
 }
 
 $page_gen_left = true;
 include("includes/page.php");
 
 /* Shorthand for getting request data */
-function has($var)  { return isset($_REQUEST[$var]); }
-function take($var) { return $_REQUEST[$var]; }
-?>
+function has($var)
+{
+    return isset($_REQUEST[$var]);
+}
+function take($var)
+{
+    return $_REQUEST[$var];
+}
--- scramble.php
+++ scramble.php
@@ -4,68 +4,77 @@ require_once("common/find-a-word.php");
 $scramble = new cls_scrambler;
 
 /* Interface here largely copied from from index.php */
-if(has('submit')) {
-	if (has('word_source') && has('word_count') && has('lang')) {
-		$req_lang     = take('lang');
-		$req_word_count  = (int)take('word_count');
-		if(($req_word_count < $scramble -> c_min_words) || ($req_word_count > $scramble -> c_max_words)) {
-			die("Number of words out of range.");
-		}
-		$req_word_source = take('word_source');
-		$do = "words"; /* Time to find out word info. */
-	} elseif(has('word_list')) {
-		$req_word_list = take('word_list');
-		$req_word_list = str_replace($req_word_list, "\r\n", "\n"); /* Switch lines to \n only */
-		$req_word_list_arr = explode("\n", take('word_list')); /* Try \n first, then change to ',' if its all on one line */
-		if(count($req_word_list_arr) == 1) { $req_word_list_arr = explode(",", take('word_list')); }
-		/* Process list. Trim and remove blanks */
-		foreach($req_word_list_arr as $key => $val) {
-			$req_word_list_arr[$key] = trim($req_word_list_arr[$key]);
-			if($req_word_list_arr[$key] == "") { unset($req_word_list_arr[$key]); }
-		}
-		if(count($req_word_list_arr) < $scramble -> c_min_words || count($req_word_list_arr) > $scramble -> c_max_words) {
-			die("Too many or not enough words!");
-		}
-		$word_list_str = join(",", $req_word_list_arr);
-		$do = "generate"; /* All info to make puzzle. */
-	} else {
-		$do = "nothing";
-	}
+if (has('submit')) {
+    if (has('word_source') && has('word_count') && has('lang')) {
+        $req_lang     = take('lang');
+        $req_word_count  = (int)take('word_count');
+        if (($req_word_count < $scramble -> c_min_words) || ($req_word_count > $scramble -> c_max_words)) {
+            die("Number of words out of range.");
+        }
+        $req_word_source = take('word_source');
+        $do = "words"; /* Time to find out word info. */
+    } elseif (has('word_list')) {
+        $req_word_list = take('word_list');
+        $req_word_list = str_replace($req_word_list, "\r\n", "\n"); /* Switch lines to \n only */
+        $req_word_list_arr = explode("\n", take('word_list')); /* Try \n first, then change to ',' if its all on one line */
+        if (count($req_word_list_arr) == 1) {
+            $req_word_list_arr = explode(",", take('word_list'));
+        }
+        /* Process list. Trim and remove blanks */
+        foreach ($req_word_list_arr as $key => $val) {
+            $req_word_list_arr[$key] = trim($req_word_list_arr[$key]);
+            if ($req_word_list_arr[$key] == "") {
+                unset($req_word_list_arr[$key]);
+            }
+        }
+        if (count($req_word_list_arr) < $scramble -> c_min_words || count($req_word_list_arr) > $scramble -> c_max_words) {
+            die("Too many or not enough words!");
+        }
+        $word_list_str = join(",", $req_word_list_arr);
+        $do = "generate"; /* All info to make puzzle. */
+    } else {
+        $do = "nothing";
+    }
 } else {
-	$do = "nothing";
+    $do = "nothing";
 }
 
 $page_title    = "Mike's Word Scrambler";
-switch($do) {
-	case "nothing":
-		$page_script = "scramble-main";
-		break;
-	case "words":
-		/* Use find-a-word dict code */
-		$find_a_word = new cls_find_a_word;
-		if($req_word_source == "dict") {
-			if(!$find_a_word -> load_dictionary($req_lang)) { /* Load dictionary */
-				die("Could not load the dictionary for that language.");
-			}
-			$find_a_word -> load_words(NULL, $req_word_count);
-		} else {
-			$find_a_word -> load_words(Array(), 0);
-		}
-		$word_list_str = join("\n", $find_a_word -> words);
-		unset($find_a_word); /* Done! */
-		$page_script = "scramble-words";
-		break;
-	case "generate":
-		$scramble -> scramble($req_word_list_arr);
-		$page_script = "scramble-generate";
-		break;
-	default:
-		die("Didn't recognise: $do");
+switch ($do) {
+    case "nothing":
+        $page_script = "scramble-main";
+        break;
+    case "words":
+        /* Use find-a-word dict code */
+        $find_a_word = new cls_find_a_word;
+        if ($req_word_source == "dict") {
+            if (!$find_a_word -> load_dictionary($req_lang)) { /* Load dictionary */
+                die("Could not load the dictionary for that language.");
+            }
+            $find_a_word -> load_words(null, $req_word_count);
+        } else {
+            $find_a_word -> load_words(array(), 0);
+        }
+        $word_list_str = join("\n", $find_a_word -> words);
+        unset($find_a_word); /* Done! */
+        $page_script = "scramble-words";
+        break;
+    case "generate":
+        $scramble -> scramble($req_word_list_arr);
+        $page_script = "scramble-generate";
+        break;
+    default:
+        die("Didn't recognise: $do");
 }
 $page_gen_left = true;
 include("includes/page.php");
 
 /* Shorthand for getting request data */
-function has($var)  { return isset($_REQUEST[$var]); }
-function take($var) { return $_REQUEST[$var]; }
-?>
+function has($var)
+{
+    return isset($_REQUEST[$var]);
+}
+function take($var)
+{
+    return $_REQUEST[$var];
+}
