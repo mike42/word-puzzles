37ad4d0cf5419f0b18ff7252b27dda1bc5c5b641
Michael Billington <michael.billington@gmail.com>
2016-06-05 17:21:39 +1000

    auto-format CSS & JS

--- client/js/word-puzzles.js
+++ client/js/word-puzzles.js
@@ -1,8 +1,8 @@
 /* Some basic option toggling code */
 var on = Array();
 function toggle(id) {
-	if ( typeof on[id] !== "undefined") {
-		if(on[id]) {
+	if (typeof on[id] !== "undefined") {
+		if (on[id]) {
 			show = false;
 		} else {
 			show = true;
@@ -11,17 +11,17 @@ function toggle(id) {
 		show = true;
 	}
 
-	if(show) {
+	if (show) {
 		on[id] = true;
-		document.getElementById(id).style.display='block';
-		document.getElementById(id + "-show").style.display='none';
-		document.getElementById(id + "-hide").style.display='block';
-		document.getElementById(id + "-sub").style.display='none';
+		document.getElementById(id).style.display = 'block';
+		document.getElementById(id + "-show").style.display = 'none';
+		document.getElementById(id + "-hide").style.display = 'block';
+		document.getElementById(id + "-sub").style.display = 'none';
 	} else {
 		on[id] = false;
-		document.getElementById(id).style.display='none';
-		document.getElementById(id + "-show").style.display='block';
-		document.getElementById(id + "-hide").style.display='none';
-		document.getElementById(id + "-sub").style.display='block';
+		document.getElementById(id).style.display = 'none';
+		document.getElementById(id + "-show").style.display = 'block';
+		document.getElementById(id + "-hide").style.display = 'none';
+		document.getElementById(id + "-sub").style.display = 'block';
 	}
 }
